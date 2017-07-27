49622de338a438f5b39b62a5b6cd99af17012fe4
Seolhee Kim <seolhee@upspringmedia.com>
2017-07-26 21:35:09 -1000

    creat user has many user

---
+++ has_many_blogs.sql
@@ -0,0 +1,11 @@
+CREATE USER has_many_user;
+
+-- CREATE DATABASE has_many_blogs OWNER has_many_user;
+
+-- CREATE TABLE users (
+--   id SERIAL,
+--   username VARCHAR(90),
+--   first_name VARCHAR(90),
+--   last_name
+
+-- );
