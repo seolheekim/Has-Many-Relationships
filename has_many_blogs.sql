4b944974c37cefc0ddb79e1f1b1b855953a22f17
Seolhee Kim <seolhee@upspringmedia.com>
2017-07-27 20:02:08 -1000

    added FK to post and comments

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,34 +1,42 @@
-CREATE USER has_many_user;
+
+DROP DATABASE IF EXISTS has_many_blogs;
 DROP USER IF EXISTS has_many_user;
 
+CREATE USER has_many_user;
 CREATE DATABASE has_many_blogs OWNER has_many_user;
-DROP DATABASE IF EXISTS has_many_blogs;
 
+\c has_many_blogs
+
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(90),
   first_name VARCHAR(90) DEFAULT NULL,
   last_name VARCHAR(90) DEFAULT NULL,
-  create_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
   updated_at timestamp with time zone NOT NULL DEFAULT NOW()
 );
-DROP TABLE IF EXISTS users;
 
+DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
   id SERIAL PRIMARY KEY,
   title VARCHAR(180) DEFAULT NULL,
   url VARCHAR(510) DEFAULT NULL,
   content TEXT DEFAULT NULL,
-  create_at timestamp with time zone NOT NULL DEFAULT NOW(),
-  updated_at timestamp with time zone NOT NULL DEFAULT NOW()
+  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  users_id INTEGER REFERENCES users(id)
 );
-DROP TABLE IF EXISTS posts;
 
+DROP TABLE IF EXISTS comments;
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY,
   body VARCHAR(510) DEFAULT NULL,
-  create_at timestamp with time zone NOT NULL DEFAULT NOW(),
-  updated_at timestamp with time zone NOT NULL DEFAULT NOW()
+  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  users_id INTEGER REFERENCES users(id),
+  posts_id INTEGER REFERENCES posts(id)
+
 );
-DROP TABLE IF EXISTS comments;
+
 
