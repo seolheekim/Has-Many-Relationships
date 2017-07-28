efbf37ae66232f30588f991ca4f82f637768ecae
Seolhee Kim <seolhee@upspringmedia.com>
2017-07-27 20:05:49 -1000

    create query for all the comments fields

--- joins.sql
+++ joins.sql
@@ -17,7 +17,7 @@ SELECT username FROM users INNER JOIN posts ON posts.id = users.id WHERE posts.c
 SELECT posts.title, posts.content, users.username FROM users INNER JOIN posts ON posts.id = users.id WHERE users.created_at <= '2015-01-01';
 
 -- -- Create a query to get the all rows in the comments table, showing post title (aliased as 'Post Title'), and the all the comment's fields
--- SELECT comments.*, posts.title AS "Post Title" FROM comments INNER JOIN posts ON comments.users_id = posts.id;
+SELECT comments.*, posts.title AS "Post Title" FROM comments INNER JOIN posts ON comments.users_id = posts.id;
 
 -- --Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created before January 1, 2015
 -- SELECT comments.*, posts.title AS "post_title", posts.url AS "post_url", comments.body AS comment_body FROM comments INNER JOIN posts on comments.users_id = posts.id WHERE posts.created_at <= '2015-01-01';
